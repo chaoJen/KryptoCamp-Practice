217993b848261699444888d3e16f973d009c901d
ChaoJen <chao.jen.main@gmail.com>
2023-08-10 20:47:19 +0800

    Chapter15

---
+++ contracts/Chapter15.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2;
+
+/**
+ * Deploy & Compiler
+ *
+ * ABI(Application Binary Interface)
+ *
+ * 編譯 Solidity
+ * - ABI
+ * - Bytecode
+ */
+contract Chapter15 {
+
+}
