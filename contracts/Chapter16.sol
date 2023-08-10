15d2c3195d8fa64f405998908c7c2f4d12e6cec4
ChaoJen <chao.jen.main@gmail.com>
2023-08-10 22:51:00 +0800

    Chapter16, 17

---
+++ contracts/Chapter16.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2;
+
+/**
+ * Dev.Tools
+ *
+ * - 主網
+ * - 測試網路
+ * - 開發者網路
+ *
+ * RPC & RESTful API
+ * JavaScript JSON RPC
+ * Blockchain Nodes Connections
+ * RPC, IPC, Websocket
+ *
+ * 客戶端: Ganache
+ * Web3.js
+ * Go-Ethereum
+ *
+ * 測試框架: Truffle, Hardhat, OpenZeppelin, Solidity-Coverage, Waffle, Foundry
+ */
+contract Chapter16 {
+
+}
---
+++ contracts/Chapter17.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2;
+
+/**
+ * Contract Optimization
+ *
+ * - Gas & Gas Cost
+ * - Space Complexity Level
+ * - Time Complexity Level
+ * - Contract Level
+ */
+contract Chapter17 {
+
+}
